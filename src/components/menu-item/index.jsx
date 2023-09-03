e6fe7f8bf95b0b2aaae0604ddf28f767c68ca96d
deltabin <timerlandergg@yandex.kz>
2023-09-03 16:41:39 +0500

    😶‍🌫️ move styles to module scss

--- src/components/menu-item/index.jsx
+++ src/components/menu-item/index.jsx
@@ -1,35 +1,32 @@
 import { toast } from 'react-hot-toast';
 import { FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import styles from './menu-item.module.scss';
 
 export function MenuItem({ products }) {
 	return (
-		<div className="flex justify-between items-center bg-[#1F2125] rounded-[10px]">
-			<div className="flex items-center gap-9">
-				<img className="w-[130px] h-[80px]" src={products.imageUrl} alt={products.title} />
-				<div className="text-xl font-bold text-white max-w-[387px]">
-					<Link to={`/product/${products.id}`}>{products.title}</Link>
-				</div>
+		<div className={styles.menuItem}>
+			<div className={styles.menuItemTitle}>
+				<img src={products.imageUrl} alt={products.title} />
+				<Link className="" to={`/product/${products.id}`}>
+					{products.title}
+				</Link>
 			</div>
-			<div className="text-base w-[377px] text-[#7A7F84]">{products.description}</div>
-			<div className="flex gap-12">
-				<div className="flex gap-2.5 text-white text-xl items-center">
-					<div className="font-bold">{Math.round(products.price)}</div>т / шт
+			<div className={styles.menuItemDesc}>{products.description}</div>
+			<div className={styles.menuItemCart}>
+				<div className={styles.menuItemCartPrice}>
+					{Math.round(products.price)} <span>т / шт</span>
 				</div>
+
 				<div
 					onClick={() =>
 						toast.success('Товар добавлен в корзину!', {
-							className: ' rounded-xl bg-[#1F2125] text-[#7A7F84]',
+							className: 'toast',
 						})
 					}
-					style={{
-						borderRadius: '22px 0px 0px 22px',
-						background: 'linear-gradient(180deg, #FDB806 0%, #FD8006 100%)',
-						boxShadow: '0px 4px 13px 0px rgba(255, 126, 0, 0.60)',
-					}}
-					className="cursor-pointer flex items-center gap-2.5 py-3 px-[22px]"
+					className={styles.menuItemCartButton}
 				>
-					<div className="font-medium text-base">в корзину</div> <FaShoppingCart />
+					<span>в корзину</span> <FaShoppingCart />
 				</div>
 			</div>
 		</div>
