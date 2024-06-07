46aaa9fa75f39d93d1775b5546a28f6f5df5a39c
cloudsync. <timerlandergg@yandex.kz>
2024-06-07 10:15:54 +0500

    Update index.jsx

--- src/pages/createProducts/index.jsx
+++ src/pages/createProducts/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { toast } from 'react-hot-toast';
 import { Link } from 'react-router-dom';
+import { toast } from 'sonner';
 import { useCreateProductsMutation } from '../../store/api';
 import styles from './createProducts.module.scss';
 
